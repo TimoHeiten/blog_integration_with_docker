96045c6d8b8c42eae3d85600c446c7f2a921640e
tih@hdk <timoheiten@googlemail.com>
2018-09-06 11:58:37 +0200

    Update integration.sh
    remvoed sleep and used a polling approach in the container

--- integration.sh
+++ integration.sh
@@ -3,8 +3,19 @@
 # docker compose up with -d flag for in background
 docker-compose up --detach
 
-# simple wait for compose to run to end
-sleep 1.5s
+# poll if db exists
+canConnect=false
+while [ "$canConnect"=false ] ; do
+	result=$(docker container exec -i mercato_db psql -h localhost -U integration_user -d integration_db -lqt | cut -f 1 -d \| | grep -e "integration_db")
+	printf "."
+ 	if [[ "integration_db"="$result" ]]
+	then	
+ 		canConnect=true
+		break
+ 	fi	
+done
+
+# if it does not start properly or in more complex scenarios use also polling for the api with a curl command
 
 # insert 3 entities and print status code to stdout
 
@@ -15,7 +26,7 @@ echo ""
 curl -s -o /dev/null -w "%{http_code}" -X POST -d '{"identifier" : "entity_3", "isactive" : true}' -H "Cache-Control: no-cache" -H "Content-Type: application/json" http://localhost:5000/api/values
 echo ""
 
-# get model values and pritn to stdout
+# get model values and print to stdout
 model=$(curl http://localhost:5000/api/values)
 echo $model
 
